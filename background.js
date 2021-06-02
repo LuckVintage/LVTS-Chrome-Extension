04704f9b868ea356d98560b532ff906e878d5250
LuckVintage <54641441+LuckVintage@users.noreply.github.com>
2021-06-02 10:14:43 +0100

    Added v2.0 files

--- background.js
+++
@@ -1,5 +0,0 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var action_url = "https://luckvintage.net";
-  chrome.tabs.update(tab.id, {url: action_url});
-});
